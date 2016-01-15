13be5f1af71e435ec76a0ef8edd444f23b8fdc04
Mark Koni Hamilton Wright <mhwright@stanford.edu>
2016-01-15 12:24:38 -0800

    Defined some data structures and added window setup code to random-forest.cpp
    Have not tested compilation or execution. None of the actual tree part
    is implemented yet. Still working on the code that is above and flanking
    the tree building and evaluation stages.
    
    Decided on explicitly building trees in actual tree structures rather
    than implicitly building them on the recursive call stack as RFMIX
    version 1 does. The trees will be built by fully classifying the
    reference data, then the query samples will be evaluated by these
    trees. The point of this is to allow future expansion/development
    into pre-training trees and storing them in a file, then using these
    pre-trained trees to run the program without an explicit set of
    reference haplotypes, just the tree file. Actual implementation of
    this is deferred in planned version 2 development. This will likely
    be a version 3 feature, but hopefully won't require a full rewrite
    of the code.

--- random-forest.cpp
+++ random-forest.cpp
@@ -8,6 +8,7 @@
 
 #include "kmacros.h"
 #include "rfmix.h"
+#include "mm.h"
 
 extern rfmix_opts_t rfmix_opts;
 
@@ -20,10 +21,148 @@ typedef struct {
   pthread_mutex_t lock;
 } thread_args_t;
 
+typedef struct {
+  int sample_idx;
+  int *haplotype[4];
+  double est_p[4];
+} wsample_t;
+
+typedef struct {
+  int n_snps;
+  snp_t *snps;
+
+  int n_query_samples;
+  wsample_t *query_samples;
+
+  int n_ref_haplotypes;
+  int **ref_haplotypes;
+  double **current_p;
+} window_t;
+
+/* In version 2 of RFMIX, an explicit tree structure is built classifying the
+   reference haplotypes only, and then the query haplotypes are evaluated on
+   this tree. This is in contrast to RFMIX version 1 never actually building
+   an explicit tree but having an ephemeral one exist on the recursive call
+   stack which is destroyed as the stack unwinds. This is to allow, ultimately,
+   pre-training of the algorithm and storing the trees in serialized form in
+   a file. In version 2 however I do not have plans to actually implement this,
+   only to allow adaptation to support it in the future. */
+typedef struct {
+  /* snp_id is the index of the SNP that divides decendents into left or right.
+     0 goes left, 1 goes right. */
+  int snp_id;
+  struct node *left;
+  struct node *right;
+
+  /* This is the sum of the current_p for each remaining reference haplotype.
+     p is never normalized to sum to one, not even at node termination. Thus,
+     if on this tree 10 reference haplotypes support assignment of a query
+     haplotype falling at the terminal node for this tree, it counts 10 times
+     as much as another tree for which only one reference haplotype is 
+     supporting subpop assignment at the terminal node. Note that each query
+     haplotype evaluated on the tree can only fall at exactly one terminal
+     node of the tree */
+  double *p;
+} node_t;
+
+typedef struct {
+  int **haplotypes;
+  int n_haplotypes;
+
+  double *p;
+  node_t *root;
+
+  /* These fields are needed during building of the tree, but are not required
+     once the tree is built. */
+  int n_remain;
+  int *remain_q;
+  int n_snps;
+  int *snplist;
+} tree_t;
+  
+/* information that does not change as the tree progresses is in tree_t, except for the
+   est_p field which is the ultimate result of the tree. est_p is merged into est_p in 
+   wsample_t when grow_tree returns from the top most call. The information that changes
+   on every decent call is stored in node_t structs. It is not implemented as a class
+   for the reasons given above, and as a result there is some manual work to create new
+   nodes.*/
+static void grow_tree(tree_t *tree, node_t *node) {
+
+}
+
+/* Unpacks alleles from sample_t haplotypes into a copy here in ints (for speed) and
+   also creates the two additional flipped phase haplotypes to analyze */
+static void setup_query_sample(wsample_t *wsample, sample_t *sample,
+			       int snp_start, int snp_end, int crf_snp,
+			       mm *ma) {
+  int k, t, s;
+
+  for(k=0; k < 4; k++)
+    wsample->haplotype[k] = (int *) ma->allocate(sizeof(int)*(snp_end - snp_start + 1), WHEREFROM);
+  
+  /* Haplotypes 0 and 1 in wsample are phased as given in input->sample[i] */
+  for(k=0; k < 2; k++) {
+    for(s=snp_start,t=0; s <= snp_end; s++,t++)
+      wsample->haplotype[k][t] = sample->haplotype[k][s];
+  }
+
+  /* Haplotypes 2 and 3 in wsample are flipped phase at the crf_window snp_idx */
+  for(k=0; k < 2; k++) {
+    for(s=snp_start,t=0; s < crf_snp; s++,t++)
+      wsample->haplotype[k+2][t] = sample->haplotype[k][s];
+
+    /* second half of window is reverse phase */
+    int l = k == 0 ? 1 : 0;
+    for(s=crf_snp; s <= snp_end; s++,t++) 
+      wsample->haplotype[k+2][t] = sample->haplotype[l][s];
+  }
+  
+  for(k=0; k < 4; k++) wsample->est_p[k] = 0.;
+}
+
+static void setup_ref_haplotype(int **ref_haplotypes, double **current_p,
+				sample_t *sample, int snp_start, int snp_end,
+				mm *ma) {
+  int s, t;
+
+  ref_haplotypes[0] = (int *) ma->allocate(sizeof(int)*(snp_end - snp_start + 1), WHEREFROM);
+  ref_haplotypes[1] = (int *) ma->allocate(sizeof(int)*(snp_end - snp_start + 1), WHEREFROM);
+  for(s=snp_start,t=0; s <= snp_end; s++,t++) {
+    ref_haplotypes[0][t] = input->samples[i].haplotypes[0][s];
+    ref_haplotypes[1][t] = input->samples[i].haplotypes[1][s];
+  }
+  for(int k=0; k < input->n_subpops; k++) {
+    current_p[0][k] = input->samples[i].current_p[0][w][k];
+    current_p[1][k] = input->samples[i].current_p[1][w][k]
+  }
+}
+
+				
 static void *random_forest_thread(void *targ) {
   thread_args_t *args = (thread_args_t *) targ;
   input_t *input = args->input;
+  window_t window;
+  int i;
+  
+  window.n_query_samples = 0;
+  for(i=0; i < input->n_samples; i++) {
+    if (input->samples[i].apriori_subpop == 0)
+      window.n_query_samples++;
+    else
+      window.n_ref_haplotypes += 2;
+  }
 
+  /* This memory allocation will not need to be done and redone with every window.
+     These fields will not change in length and can be reused. The haplotypes 
+     themselves though may change with each window if the rf window size is 
+     variable. Those are allocated in the loop using mm->allocate() */
+  MA(window.query_samples, sizeof(wsample_t)*window.n_query_samples, wsample_t);
+  MA(window.ref_haplotypes, sizeof(int *)*window.n_ref_haplotypes, int *);
+  MA(window.current_p, sizeof(double *)*window.n_ref_haplotypes, double *);
+  MA(window.current_p[0], sizeof(double)*window.n_ref_haplotypes*input->n_subpops, double);
+  for(i=1; i < input->n_subpops; i++)
+    window.current_p[i] = window.current_p[i-1] + input->n_subpops;
+  
   /* args object is always locked at the loop start point or when loop exits */
   pthread_mutex_lock(&args->lock);
   for(;;) {
@@ -34,10 +173,29 @@ static void *random_forest_thread(void *targ) {
     args->next_window = end_window;
     pthread_mutex_unlock(&args->lock);
 
-    for(int window=start_window; window < end_window; window++) {
+    mm *ma = new mm(1024*1024, WHEREFROM);
+    for(int w=start_window; w < end_window; w++) {
+      crf_window_t *crf = input->crf_windows + w;
       
     /* set up window_t object and decoded/unpacked information from the input_t object */
+      window.n_snps = crf->rf_end_idx - crf->rf_start_idx + 1;
+      window.snps = input->snps + crf->rf_start_idx;
 
+      int q = 0;
+      int r = 0;
+      for(i=0; i < input->n_samples; i++) {
+	if (input->samples[i].apriori_subpop == 0) {
+	  window.query_samples[q].sample_idx = i;
+	  setup_query_sample(window.query_samples + q, input->samples + i,
+			     crf->rf_start_idx, crf->rf_end_idx, crf->snp_idx, ma);
+	  q++;
+	} else {
+	  setup_ref_haplotype(window.ref_haplotypes + r, window.current_p + r,
+			      input->samples + i, crf->rf_start_idx, crf->rf_end_idx, ma)
+	  r += 2;
+	}
+      }
+	
     /* Call window function to process the window */
 
     /* Repack window_t object results into input_t and reset/reinitialize window_t 
