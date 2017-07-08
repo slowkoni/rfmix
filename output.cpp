c8fed45dbb5f5bde55c190057ad1c79512e5e356
David Riccardi <driccardi@embarkvet.com>
2017-07-08 11:59:57 -0400

    adds comments that explain .tsv file format

--- output.cpp
+++ output.cpp
@@ -84,7 +84,6 @@ void msp_output(input_t *input) {
     fprintf(stderr,"Can't open output file %s (%s)\n", fname, strerror(errno));
     exit(-1);
   }
-  
   fprintf(f,"#");
   fprintf(f,"Subpopulation order/codes: %s=0", input->reference_subpops[0]);
   for(int i=1; i < input->n_subpops; i++) {
@@ -123,6 +122,34 @@ static void fb_output_haplotype(FILE *f, int16_t *p, int n) {
 
 #define FB_EXTENSION ".fb.tsv"
 void fb_output(input_t *input) {
+// the output is the tab separated file \<output basename\>.fb.tsv
+
+// The first line is a comment line, that specifies the order of subpopulations:
+// eg:
+
+//   #reference_panel_population: golden_retriever  labrador_retriever  poodle  poodle_small
+
+// The second line specifies the column names, and every following lines gives data on a chunk of the genome, called a conditional random field (CRF) point.
+
+// The first few columns specifies the chromosome, genetic marker's physical position in basepair units and genetic position in centiMorgans, and the genetic marker's numerical index in the rfmix genetic map input file. The remaining columns give the probabilities that the CRF point for a genotype's haplotype was assigned to a specific reference panel population. A genotype has two haplotypes, so the number of probabilities for a genotype is 2*(number of reference panel populations). The number of columns in the file is 4 + (number of genotypes) * 2 * (number of reference panel populations.
+
+// For example, for a rfmix run with 2 admixed genotype_ids run against 3 reference panel populations, the columns would be::
+
+
+//   chromosome physical_position genetic_position genetic_marker_index 
+//   genotype_id1:::hap1:::subpop1:::probability
+//   genotype_id1:::hap1:::subpop2:::probability
+//   genotype_id1:::hap1:::subpop3:::probability
+//   genotype_id1:::hap2:::subpop1:::probability
+//   genotype_id1:::hap2:::subpop2:::probability
+//   genotype_id1:::hap2:::subpop3:::probability
+//   genotype_id2:::hap1:::subpop1:::probability
+//   genotype_id2:::hap1:::subpop2:::probability
+//   genotype_id2:::hap1:::subpop3:::probability
+//   genotype_id2:::hap2:::subpop1:::probability
+//   genotype_id2:::hap2:::subpop2:::probability
+//   genotype_id2:::hap2:::subpop3:::probability
+
   fprintf(stderr,"Outputing forward-backward results.... \n");
   int fname_length = strlen(rfmix_opts.output_basename) + strlen(FB_EXTENSION) + 1;
   char fname[fname_length];
@@ -135,21 +162,21 @@ void fb_output(input_t *input) {
   }
   
   fprintf(f,"#");
-  fprintf(f,"Subpopulation_order:\t%s", input->reference_subpops[0]);
+  fprintf(f,"reference_panel_population:\t%s", input->reference_subpops[0]);
   for(int i=1; i < input->n_subpops; i++) {
     fprintf(f,"\t%s", input->reference_subpops[i]);
   }
   fprintf(f,"\n");
-  fprintf(f,"chromosome\tphysical_pos_in_bp\tgenetic_position_in_cm\tgenetic_marker_index");
+  fprintf(f,"chromosome\tphysical_position\tgenetic_position\tgenetic_marker_index");
   for(int j=0; j < input->n_samples; j++) {
     sample_t *sample = input->samples + j;
     if (sample->apriori_subpop != -1 || sample->s_sample == 1) continue;
 
     for(int k=0; k < input->n_subpops; k++) {
-      fprintf(f, "\t%s:::hap1:::%s", sample->sample_id, input->reference_subpops[k]);
+      fprintf(f, "\t%s:::hap1:::%s:::probability", sample->sample_id, input->reference_subpops[k]);
     }
     for(int k=0; k < input->n_subpops; k++) {
-      fprintf(f, "\t%s:::hap2:::%s", sample->sample_id, input->reference_subpops[k]);
+      fprintf(f, "\t%s:::hap2:::%s:::probability", sample->sample_id, input->reference_subpops[k]);
     }
 
     // fprintf(f,"\t%s.0\t%s.1", sample->sample_id, sample->sample_id);
